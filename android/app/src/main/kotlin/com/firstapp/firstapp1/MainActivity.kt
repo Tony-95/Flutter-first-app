13d8ab3d1659a3b7970c11aa20ccc2dfa674307b
Tony <tony_osei@hotmail.com>
2021-04-11 22:41:12 +0200

    flutter fisrt project

---
+++ android/app/src/main/kotlin/com/firstapp/firstapp1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.firstapp.firstapp1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/productList.dart
@@ -0,0 +1,6 @@
+import 'package:firstapp1/AppColors.dart';
+import 'package:flutter/material.dart';
+import 'package:firstapp1/widgets/appbar.dart';
+import 'package:firstapp1/widgets/button.dart';
+import 'package:firstapp1/widgets/circle.dart';
+
